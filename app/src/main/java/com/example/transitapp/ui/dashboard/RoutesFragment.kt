28f220fd90b17ea207d5b4d840553464b5c53d71
kenzie0436126 <w0436126@nscc.ca>
2023-12-01 10:15:51 -0400

    Phase 4 step 1

--- app/src/main/java/com/example/transitapp/ui/dashboard/RoutesFragment.kt
+++ app/src/main/java/com/example/transitapp/ui/dashboard/RoutesFragment.kt
@@ -1,17 +1,22 @@
 package com.example.transitapp.ui.dashboard
 
 import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.ScrollView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.transitapp.R
 import com.example.transitapp.databinding.FragmentRoutesBinding
+import org.w3c.dom.Text
 import java.io.File
 
 class RoutesFragment : Fragment() {
@@ -30,7 +35,6 @@ class RoutesFragment : Fragment() {
         _binding = FragmentRoutesBinding.inflate(inflater, container, false)
         val root: View = binding.root
         val addButton: Button = binding.addBtn;
-        val routesText: TextView = binding.textViewMyRoutes
 
 
 
@@ -43,35 +47,75 @@ class RoutesFragment : Fragment() {
         val filename = "RoutesFile"
         val file = File(context?.filesDir, filename)
 
-        if (!file.exists()) {
+
+        if (file.exists()) {
             context?.openFileOutput(filename, Context.MODE_PRIVATE).use {
             }
         }
 
         readDisplayFile(filename);
 
-        addButton.setOnClickListener{
+        addButton.setOnClickListener {
             val routeSelected = auto.text.toString();
-
-            val appendText = "$routeSelected,";
-            context?.openFileOutput(filename, Context.MODE_APPEND).use {
-                it?.write(appendText.toByteArray())
+            val fileText = context?.openFileInput(filename)?.bufferedReader()?.useLines { lines ->
+                lines.fold("") { some, text ->
+                    "$some\n$text"
+                }
             }
-            readDisplayFile(filename)
+
+            if (fileText == "") {
+                var appendText = routeSelected;
+                context?.openFileOutput(filename, Context.MODE_APPEND).use {
+                    it?.write(appendText.toByteArray())
+                    readDisplayFile(filename)
+                }
+            } else {
+                val routesArray: List<String>? = fileText?.split(",")
+                var inRoutes = false;
+                if (routesArray != null) {
+                    for (route in routesArray) {
+                        if (route == routeSelected) {
+                            inRoutes = true;
+                            break;
+                        }
+                    }
+                    if (!inRoutes) {
+                        var appendText = ",$routeSelected";
+                        context?.openFileOutput(filename, Context.MODE_APPEND).use {
+                            it?.write(appendText.toByteArray())
+                        }
+                        readDisplayFile(filename)
+                    }
+                }
+            }
         }
         return root
     }
 
     private fun readDisplayFile(fileName:String) {
-        val routesText: TextView = binding.textViewMyRoutes
+
         val fileText = context?.openFileInput(fileName)?.bufferedReader()?.useLines { lines ->
             lines.fold("") { some, text ->
                 "$some\n$text"
             }
         }
+
+        val routeLayout: LinearLayout = binding.routeLayout;
+        routeLayout.removeAllViews()
+
         if (fileText != null) {
-            val fileTextsplit = fileText
-            routesText.text = fileText
+            if (fileText != "") {
+                val splitFileText = fileText.split(",")
+
+                for (item in splitFileText) {
+                    val routeText = TextView(requireContext())
+                    routeText.text = item
+                    routeText.textSize = 28f // Set the text size in scaled pixels
+                    routeText.setTextColor(Color.BLACK)
+                    routeLayout.addView(routeText)
+                }
+                Log.i("testing", fileText.toString())
+            }
         }
     }
 
